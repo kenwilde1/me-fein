b1926c69606b5a8c536d5dcc9a9106a8cbc8fb3b
Kenny Wilde <kenwilde@TWRH2JKNQK.local>
2024-02-07 21:46:43 +0000

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,23 @@
 import "@/styles/globals.css";
-import { Flowbite, CustomFlowbiteTheme } from "flowbite-react";
 import type { AppProps } from "next/app";
 import Header from "./header";
-import { Roboto } from 'next/font/google'
+import { Roboto } from "next/font/google";
 
-console.warn = () => { };
+import { Analytics } from "@vercel/analytics/react";
+
+console.warn = () => {};
 
 const roboto = Roboto({
-  weight: ['400'],
-  subsets: ['latin'],
-})
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${roboto.className}`}>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
     </div>
-  )
+  );
 }
