ee6aa62b8d39d15be958e160b3ee3aa1faa77d92
Kenny Wilde <kenwilde@TWRH2JKNQK.local>
2024-02-07 22:11:38 +0000

    add styling to mobile

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./helpers/contact_form";
 
 export default function Contact() {
   return (
-    <div className="contact">
+    <div id="contact">
       <h1>Contact Me</h1>
       <div>
         <ContactForm />
--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -2,31 +2,41 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function Header(): JSX.Element {
+  const router = useRouter();
 
-    const router = useRouter();
+  const isActive = (page: string): boolean => {
+    return page === router.pathname;
+  };
 
-    const isActive = (page: string): boolean => {
-        return page === router.pathname;
-    }
-
-    return (
-        <nav id='navbar'>
-            <div id='navbar-content'>
-                <ul>
-                    <Link href='/' className={isActive('/') ? 'active-page' : ''}>home</Link>
-                    <Link href='/about' className={isActive('/about') ? 'active-page' : ''}>about</Link>
-                    <Link href='/contact' className={isActive('/contact') ? 'active-page' : ''}>contact</Link>
-                </ul>
-                <div className='toggle-theme'>
-                    <div>
-                    </div>
-                </div>
-                <div className='socials'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav id="navbar">
+      <div id="navbar-content">
+        <ul>
+          <Link href="/" className={isActive("/") ? "active-page" : ""}>
+            home
+          </Link>
+          <Link
+            href="#about"
+            className={isActive("/about") ? "active-page" : ""}
+          >
+            about
+          </Link>
+          <Link
+            href="#contact"
+            className={isActive("/contact") ? "active-page" : ""}
+          >
+            contact
+          </Link>
+        </ul>
+        <div className="toggle-theme">
+          <div></div>
+        </div>
+        <div className="socials">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </nav>
+  );
 }
